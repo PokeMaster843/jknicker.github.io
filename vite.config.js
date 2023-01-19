6cab00a0b9128edaace2c9c9dfbe70422b4f4ecd
PokeMaster843 <pokemaster843@gmail.com>
2023-01-19 16:30:34 -0500

    minor config changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import * as path from 'path'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'node:url'
+import vue from 'vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: [
-        fileURLToPath(new URL('./src/assets/job_profile.jpg', import.meta.url))
-      ]
-    },
-  },
 })
