a18bcc9f4bbe02fb2cd03bff38d38d526a436899
PokeMaster843 <pokemaster843@gmail.com>
2023-01-18 20:46:48 -0500

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: [
+        fileURLToPath(new URL('src/assets/job_profile.jpg', import.meta.url))
+      ]
+    },
+  },
 })
